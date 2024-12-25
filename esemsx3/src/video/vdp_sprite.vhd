3048dada2d78a5248d69e1af6e6d18a082d91fc3
KdL <gnogni74@gmail.com>
2024-12-25 06:46:23 +0100

    Update vdp_sprite.vhd

--- esemsx3/src/video/vdp_sprite.vhd
+++ esemsx3/src/video/vdp_sprite.vhd
@@ -360,8 +360,8 @@ BEGIN
         IF( RESET = '1' )THEN
             FF_SP_EN <= '0';
         ELSIF( CLK21M'EVENT AND CLK21M = '1' )THEN
-            IF( DOTSTATE = "01" AND DOTCOUNTERX = 0 )THEN
-                FF_SP_EN <= W_ACTIVE;
+            IF( DOTSTATE = "01" AND DOTCOUNTERX = 256+8 )THEN                   -- Fixed by KdL, 2024/Dec/25th
+                FF_SP_EN <= (NOT REG_R8_SP_OFF) AND W_ACTIVE;
             END IF;
         END IF;
     END PROCESS;
@@ -508,7 +508,7 @@ BEGIN
                     END IF;
                 WHEN SPSTATE_YTEST_DRAW =>
                     IF( DOTCOUNTERX = 256+8 )THEN
-                        SPVRAMACCESSING <= (NOT REG_R8_SP_OFF) AND FF_SP_EN;
+                        SPVRAMACCESSING <= FF_SP_EN;
                     END IF;
                 WHEN SPSTATE_PREPARE =>
                     IF( SPPREPAREEND = '1' )THEN
